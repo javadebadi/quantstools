7c60d939fddcc5acaf287e63feaeaad3a0f1f35f
javadebadi <javad.ebadi.1990@gmail.com>
2022-04-16 20:51:20 +0430

    Added new method and attributes to Order class

--- quantstools/order.py
+++ quantstools/order.py
@@ -7,13 +7,18 @@ class Order:
 
     def __init__(
         self,
-        symbol,
-        side,
-        price,
+        symbol: str,
+        side: str,
+        price: Price,
+        amount: str,
+        type_: str = 'LIMIT',
         ):
         self.symbol = symbol
         self.side = side
         self.price = price
+        self.amount = amount
+        self.type_ = type_
+
 
     @property
     def symbol(self) -> str:
@@ -34,20 +39,43 @@ class Order:
         self._side = side
 
     @property
-    def price(self):
+    def price(self) -> Price:
         return self._price
 
     @price.setter
-    def price(self, price):
+    def price(self, price) -> None:
         assert isinstance(price, Price)
         self._price = price
 
-    def get_price(self):
+    @property
+    def amount(self) -> str:
+        return self._amount
+
+    @amount.setter
+    def amount(self, amount) -> None:
+        assert type(amount) == str
+        self._amount = amount
+
+    @property
+    def type_(self) -> str:
+        return self._type_
+
+    @type_.setter
+    def type_(self, type_) -> None:
+        assert type_ in ['LIMIT', 'MARKET']
+        self._type_ = type_
+
+    def get_price(self) -> Price:
         return self.price.get_price()
 
-    def to_dict(self) -> dict:
+    def to_dict(self, numeric=False) -> dict:
         d = {}
         d['symbol'] = self.symbol
         d['side'] = self.side
         d['price'] = self.get_price()
+        d['amount'] = self.amount
+        if numeric is True:
+            d['price'] = float(d['price'])
+            d['amount'] = float(d['amount'])
+        d['type_'] = self.type_
         return d
--- tests/test_order.py
+++ tests/test_order.py
@@ -6,27 +6,66 @@ from quantstools.order import Order
 class TestOrder:
 
     def test_symbol_property(self):
-        o = Order('ETH-BTC', 'BUY', Price(0.12, 5, 4))
+        o = Order('ETH-BTC', 'BUY', Price(0.12, 5, 4), '0.05')
         assert o.symbol == 'ETH-BTC'
 
     def test_symbol_property_raises_assertion_error(self):
         with pytest.raises(AssertionError) as exc_info:
-            Order([], 'BUY', Price(0.12, 5, 4))
+            Order([], 'BUY', Price(0.12, 5, 4), '0.05')
         with pytest.raises(AssertionError) as exc_info:
-            Order(12, 'BUY', Price(0.12, 5, 4))
+            Order(12, 'BUY', Price(0.12, 5, 4), '0.05')
 
     def test_side_property(self):
-        o = Order('ETH-BTC', 'BUY', Price(0.12, 5, 4))
+        o = Order('ETH-BTC', 'BUY', Price(0.12, 5, 4), '0.05')
         assert o.side == 'BUY'
-        o = Order('ETH-BTC', 'SELL', Price(0.12, 5, 4))
+        o = Order('ETH-BTC', 'SELL', Price(0.12, 5, 4), '0.05')
         assert o.side == 'SELL'
 
     def test_side_property_raises_assertion_error(self):
         with pytest.raises(AssertionError) as exc_info:
-            Order('ETH-BTC', 'xyz', Price(0.12, 5, 4))
+            Order('ETH-BTC', 'xyz', Price(0.12, 5, 4), '0.05')
         with pytest.raises(AssertionError) as exc_info:
-            Order('ETH-BTC', None, Price(0.12, 5, 4))
+            Order('ETH-BTC', None, Price(0.12, 5, 4), '0.05')
+
+    def test_price_property(self):
+        o = Order('ETH-BTC', 'BUY', Price(0.12, 5, 4), '0.05')
+        assert o.price == Price(0.12, 5, 4)
+
+    def test_price_property_raises_assertion_error(self):
+        with pytest.raises(AssertionError) as exc_info:
+            Order('ETH-BTC', 'BUY', 0.12, '0.05')
+
+    def test_amount_property(self):
+        o = Order('ETH-BTC', 'BUY', Price(0.12, 5, 4), '0.05')
+        assert o.amount == '0.05'
+
+    def test_amount_property_raises_assertion_error(self):
+        with pytest.raises(AssertionError) as exc_info:
+            Order('ETH-BTC', 'BUY', Price(0.12, 5, 4), 0.05)
+
+    def test_type__property(self):
+        o = Order('ETH-BTC', 'BUY', Price(0.12, 5, 4), '0.05', 'LIMIT')
+        assert o.type_ == 'LIMIT'
+        o = Order('ETH-BTC', 'BUY', Price(0.12, 5, 4), '0.05', 'MARKET')
+        assert o.type_ == 'MARKET'
+
+    def test_type__property_raises_assertion_error(self):
+        with pytest.raises(AssertionError) as exc_info:
+            Order('ETH-BTC', 'BUY', Price(0.12, 5, 4), 0.05, 'xyz')
 
     def test_to_dict(self):
-        o = Order('ETH-BTC', 'BUY', Price(0.12, 5, 4))
-        assert o.to_dict() == {'side': 'BUY', 'symbol': 'ETH-BTC', 'price': '0.1200'}
+        o = Order('ETH-BTC', 'BUY', Price(0.12, 5, 4), '0.05', 'LIMIT')
+        assert o.to_dict() == {
+            'side': 'BUY',
+            'symbol': 'ETH-BTC',
+            'price': '0.1200',
+            'amount': '0.05',
+            'type_': 'LIMIT',
+            }
+        assert o.to_dict(numeric=True) == {
+            'side': 'BUY',
+            'symbol': 'ETH-BTC',
+            'price': 0.12,
+            'amount': 0.05,
+            'type_': 'LIMIT',
+            }
